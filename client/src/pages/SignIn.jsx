a4f8a87f2ca126297c5893e1a4605b3e06aa408c
dixon <dixonkelvin98@gmail.com.com>
2024-06-01 12:24:37 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+  
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value });
 
@@ -14,8 +20,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -25,17 +30,17 @@ const SignIn = () => {
   });
       
       const data = await res.json();
-      setLoading(false);
-      if (data.success === false){
-        setError(true);
+       
+      if (data.success === false) {
+     dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
      navigate('/');
     } catch (error) {
-     setLoading(false);
-     setError(true);
+     dispatch(signInFailure(error));
     }
-  }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
      <h1 className='text-3xl text-center font-semibold my-7'>
@@ -65,7 +70,9 @@ const SignIn = () => {
       <span className="text-blue-500">Sign in</span>
     </Link>
     </div>
-    <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
+    <p className="text-red-700 mt-5">
+      {error ? error.message || 'Something went wrong!' : ''}
+      </p>
     </div>
   ) 
 }
