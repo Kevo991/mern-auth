344403133050e1cf0243eade520f634e34c3f178
dixon <dixonkelvin98@gmail.com.com>
2024-06-04 10:59:48 +0100

    create and add OAuth component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
+/* singUp auth */
 
 export const signup = async(req, res) => {
 const { username, email, password } = req.body;
@@ -10,11 +11,16 @@ const hashedPassword = bcryptjs.hashSync(password, 10);
 const newUser =  new User({ username, email, password: hashedPassword });
 try {
 await newUser.save();
-res.status(201).json({ message: "User created successfully" });
+res
+.status(201)
+.json({ message: "User created successfully" });
 } catch (error) { 
  next(error); 
 }
-};  
+}; 
+
+
+/* SignIn auth */
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
@@ -35,12 +41,50 @@ export const signin = async (req, res, next) => {
     } catch (error) {
         next(error); 
     }
+    };
+/* google auth */
+
+export const google = async (req, res, next) => {
+    try{
+        const user = await User.findOne({ email: req.body.email })
+if (user) {
+    const token = jwt.sign({ id: user._id }, process.env.
+        JWT_SECRET);
+    const { password: hashedPassword, ...rest } = user._doc;
+    const expiryDate = new Date(Date.now() + 3600000);  // 1 hour
+res.cookie('access_token', token, { httpOnly: true, 
+    expires: expiryDate })
+    .status(200)
+    .json(rest);
+} else {    
+const generatedPassword = 
+Math.random().toString(36).slice(-8)
++ Math.random().toString(36).slice(-8);
+const hashedPassword = bcryptjs.hashSync
+(generatedPassword, 10); 
+const newUser = new User ({ 
+    username: req.body.name.split("")
+.join("").toLowerCase() +
+ Math.random().toString(36).slice(-8),
+ email: req.body.email,
+ password: hashedPassword, 
+ profilePicture: req.body.photo }); 
+
+await newUser.save();
+const token = jwt.sign({ id: newUser._id }, 
+    process.env.JWT_SECRET);
+    const { password: hashedPassword2, ...rest } = newUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+res
+.cookie('access_token', token, 
+{
+    httpOnly: true,
+    expires: expiryDate,
+})
+ .status(200)
+ .json(rest); 
+}
+    } catch (error) {
+next(error)
     }
- 
-
-
-
-
-
-
-
+}
