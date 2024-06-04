c4a7abcb507a21981ab5aaf92347dfe1fb86326c
dixon <dixonkelvin98@gmail.com.com>
2024-06-04 11:34:52 +0100

    update header component and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute";
 
  
 
@@ -13,15 +14,16 @@ const App = () => {
   return (
     <BrowserRouter>
     {/* header */}
-     <Header />
+     <Header /> 
    <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path="/sign-in" element={<SignIn />} />
     <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/profile" element={<Profile />} />
+    <Route element={<PrivateRoute />}> 
+     <Route path="/profile" element={<Profile />} />
+    </Route>
    </Routes>
-
     </BrowserRouter>
     
   )
